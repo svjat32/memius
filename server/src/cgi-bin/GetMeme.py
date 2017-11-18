824703f927657db8e57160cdda59bb7badc2cfed
drsanches <dr.sanches3@gmail.com>
2017-11-19 01:08:42 +0300

    Added GetMem.py, deleted GetRecommendedMem.py

--- server/src/cgi-bin/GetMeme.py
+++ server/src/cgi-bin/GetMeme.py
@@ -1,22 +1,50 @@
 import cgi
 import json
+import sqlite3
+import base64
 
 form = cgi.FieldStorage()
 
-sessionId = form.getvalue("getSessionId")
-memeId = form.getvalue("memeId")
-if sessionId is None or memeId is None:
-    status = "Failure"
-else:
-    status = "Success"
+session_id = form.getvalue("SessionId")
+meme_id = form.getvalue("MemeId")
 
-answer = json.dumps({"Status": status, "Container": {"AuthorId": "Int",
-                                                     "Meme": "Binary",
-                                                     "DataAndTime": "String",
-                                                     "LikeCount": "Int",
-                                                     "DislikeCount": "Int"
-                                                     }})
+answer = {}
+
+if session_id is None or meme_id is None:
+    answer["Status"] = "Failure"
+else:
+    connection = sqlite3.connect("ServerDB.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT User_id FROM Sessions WHERE Id = ?", (session_id,))
+    user_id = int(*cursor.fetchall()[0])
+
+    cursor.execute("SELECT Meme, Date_and_time FROM Memes WHERE Id = ?", (user_id,))
+    for d in cursor.fetchall():
+        results = bytearray(d[0])
+        meme = ""
+        for c in results:
+            meme = meme + chr(c)
+        date_and_time = d[1]
+
+    cursor.execute("SELECT * FROM Likes WHERE Meme_id = ?", (meme_id,)).rowcount
+    like_count = len(cursor.fetchall())
+
+    cursor.execute("SELECT * FROM Dislikes WHERE Meme_id = ?", (meme_id,))
+    dislike_count = len(cursor.fetchall())
+
+    connection.commit()
+    connection.close()
+
+    answer["Status"] = "Success"
+    container = {}
+    container["AuthorId"] = user_id
+    container["DateAndTime"] = date_and_time
+    container["LikeCount"] = like_count
+    container["DislikeCount"] = dislike_count
+    container["Meme"] = meme
+    answer["Container"] = container
+
 
 print("Content-type: application/json")
 print()
-print(answer)
+print(json.dumps(answer))
