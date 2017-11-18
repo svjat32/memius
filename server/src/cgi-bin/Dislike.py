fe4658902b2f7015fcc2b1f1b8733933bf7273cd
drsanches <dr.sanches3@gmail.com>
2017-11-19 02:30:46 +0300

    Like.py and Dislike.py work

--- server/src/cgi-bin/Dislike.py
+++ server/src/cgi-bin/Dislike.py
@@ -1,18 +1,44 @@
 import cgi
 import json
+import sqlite3
+import datetime
 
 form = cgi.FieldStorage()
 
-sessionId = form.getvalue("SessionId")
-memeId = form.getvalue("Int")
+session_id = form.getvalue("SessionId")
+meme_id = form.getvalue("MemeId")
 
-if sessionId is None or memeId is None:
-    status = "Failure"
+answer = {}
+
+if session_id is None or meme_id is None:
+    answer["Status"] = "Failure"
 else:
-    status = "Success"
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    day = datetime.datetime.now().day
+    hour = datetime.datetime.now().hour
+    minute = datetime.datetime.now().minute
+    second = datetime.datetime.now().second
+    date_and_time = datetime.datetime(year, month, day, hour, minute, second)
+    try:
+        connection = sqlite3.connect("ServerDB.db")
+        cursor = connection.cursor()
 
-answer = json.dumps({"Status": status})
+        cursor.execute("SELECT User_id FROM Sessions WHERE Id = ?", (session_id,))
+        user_id = int(cursor.fetchone()[0])
+
+        #TODO: Check likes
+
+        cursor.execute("INSERT INTO Dislikes (Meme_id, User_id, Date_and_time) VALUES (?, ?, ?)", (meme_id, user_id, date_and_time))
+
+        connection.commit()
+        connection.close()
+
+        answer["Status"] = "Success"
+    except:
+        answer["Status"] = "Failure"
+
 
 print("Content-type: application/json")
 print()
-print(answer)
+print(json.dumps(answer))
