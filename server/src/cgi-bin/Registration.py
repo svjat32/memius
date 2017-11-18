fe3432ccb765abfd1ce33f7adc8e733a5fa77db6
drsanches <dr.sanches3@gmail.com>
2017-11-18 18:48:13 +0300

    Registration works

--- server/src/cgi-bin/Registration.py
+++ server/src/cgi-bin/Registration.py
@@ -1,18 +1,30 @@
 import cgi
 import json
+import sqlite3
 
 form = cgi.FieldStorage()
 
 username = form.getvalue("Username")
 password = form.getvalue("Password")
 
-if username is None or password is None:
-    status = "Failure"
-else:
-    status = "Success"
+answer = {}
 
-answer = json.dumps({"Status": status})
+if username is None or password is None:
+    answer["Status"] = "Failure"
+else:
+    try:
+        connection = sqlite3.connect("ServerDB.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO Users (Username, Password) VALUES (?, ?)", (username, password))
+        results = cursor.fetchall()
+        connection.commit()
+        connection.close()
+    except:
+        answer["Status"] = "Failure"
+    else:
+        answer["Status"] = "Success"
+
 
 print("Content-type: application/json")
 print()
-print(answer)
+print(json.dumps(answer))
