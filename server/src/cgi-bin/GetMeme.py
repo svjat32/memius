66de3e93a08012a5e9ae5a002b67023e67d7fb3a
drsanches <dr.sanches3@gmail.com>
2017-11-19 01:44:59 +0300

    GetMeme.py works

--- server/src/cgi-bin/GetMeme.py
+++ server/src/cgi-bin/GetMeme.py
@@ -13,36 +13,41 @@ answer = {}
 if session_id is None or meme_id is None:
     answer["Status"] = "Failure"
 else:
-    connection = sqlite3.connect("ServerDB.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT User_id FROM Sessions WHERE Id = ?", (session_id,))
-    user_id = int(*cursor.fetchall()[0])
+    try:
+        connection = sqlite3.connect("ServerDB.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT User_id FROM Sessions WHERE Id = ?", (session_id,))
+        user_id = int(*cursor.fetchall()[0])
 
-    cursor.execute("SELECT Meme, Date_and_time FROM Memes WHERE Id = ?", (user_id,))
-    for d in cursor.fetchall():
-        results = bytearray(d[0])
-        meme = ""
-        for c in results:
-            meme = meme + chr(c)
-        date_and_time = d[1]
+        cursor.execute("SELECT Meme, Date_and_time FROM Memes WHERE Id = ?", (meme_id,))
 
-    cursor.execute("SELECT * FROM Likes WHERE Meme_id = ?", (meme_id,)).rowcount
-    like_count = len(cursor.fetchall())
+        for d in cursor.fetchall():
+            date_and_time = str(d[1])
+            results = bytearray(d[0])
+            meme = ""
+            for c in results:
+                meme = meme + chr(c)
 
-    cursor.execute("SELECT * FROM Dislikes WHERE Meme_id = ?", (meme_id,))
-    dislike_count = len(cursor.fetchall())
 
-    connection.commit()
-    connection.close()
+        cursor.execute("SELECT * FROM Likes WHERE Meme_id = ?", (meme_id,)).rowcount
+        like_count = len(cursor.fetchall())
 
-    answer["Status"] = "Success"
-    container = {}
-    container["AuthorId"] = user_id
-    container["DateAndTime"] = date_and_time
-    container["LikeCount"] = like_count
-    container["DislikeCount"] = dislike_count
-    container["Meme"] = meme
-    answer["Container"] = container
+        cursor.execute("SELECT * FROM Dislikes WHERE Meme_id = ?", (meme_id,))
+        dislike_count = len(cursor.fetchall())
+
+        connection.commit()
+        connection.close()
+
+        answer["Status"] = "Success"
+        container = {}
+        container["AuthorId"] = user_id
+        container["DateAndTime"] = date_and_time
+        container["LikeCount"] = like_count
+        container["DislikeCount"] = dislike_count
+        container["Meme"] = meme
+        answer["Container"] = container
+    except:
+        answer["Status"] = "Failure"
 
 
 print("Content-type: application/json")
